09c3e016884ef664571bc35ae9e9c81dda215dcf
ligi <ligi@ligi.de>
2016-06-05 15:15:31 +0200

    Add @JvmOverloads annotations to have a nicer java API

--- src/main/kotlin/io/ipfs/kotlin/IPFS.kt
+++ src/main/kotlin/io/ipfs/kotlin/IPFS.kt
@@ -3,7 +3,7 @@ package io.ipfs.kotlin
 import com.squareup.moshi.Moshi
 import okhttp3.*
 
-open class IPFS(val base_url: String = "http://127.0.0.1:5001/api/v0/",
+open class IPFS @JvmOverloads constructor(val base_url: String = "http://127.0.0.1:5001/api/v0/",
                 val okHttpClient: OkHttpClient = OkHttpClient.Builder().build(),
                 val moshi: Moshi = Moshi.Builder().build()) {
 
