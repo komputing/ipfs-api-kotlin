6ecb8dfc9dd45ca1033fb29de3c8adaa0e71fb39
ligi <ligi@ligi.de>
2016-06-05 14:10:42 +0200

    Add default for okhttpclient

--- src/main/kotlin/io/ipfs/kotlin/IPFS.kt
+++ src/main/kotlin/io/ipfs/kotlin/IPFS.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Moshi
 import okhttp3.*
 
 open class IPFS(val base_url: String = "http://127.0.0.1:5001/api/v0/",
-                val okHttpClient: OkHttpClient,
+                val okHttpClient: OkHttpClient = OkHttpClient.Builder().build(),
                 val moshi: Moshi = Moshi.Builder().build()) {
 
     val add by lazy { Add(this) }
