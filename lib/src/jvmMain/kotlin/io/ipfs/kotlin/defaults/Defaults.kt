5074f4a69b95d7f6d3bc2c68b2dd5a4793798520
Luca Spinazzola <mspinluca@gmail.com>
2022-05-24 16:32:40 -0400

    remove unused import

--- lib/src/jvmMain/kotlin/io/ipfs/kotlin/defaults/Defaults.kt
+++ lib/src/jvmMain/kotlin/io/ipfs/kotlin/defaults/Defaults.kt
@@ -3,14 +3,12 @@ package io.ipfs.kotlin.defaults
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 
 
 internal fun createOKHTTP() = OkHttpClient.Builder().build()
 
-@OptIn(ExperimentalSerializationApi::class)
 internal fun createKTOR() = HttpClient {
     install(ContentNegotiation) {
         json(Json {
