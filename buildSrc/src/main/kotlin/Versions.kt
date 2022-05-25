49dee73c1525ddda3e46c9c97bf8c3c59b4f7015
Luca Spinazzola <mspinluca@gmail.com>
2022-05-24 22:43:45 -0400

    move more dependencies to buildsrc versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,9 @@
 object Versions {
     const val kotlin = "1.6.21"
+    const val serialization= "1.3.2"
     const val versions_plugin = "0.28.0"
     const val jupiter = "5.6.2"
     const val okhttp = "4.9.3"
+    const val coroutines = "1.6.1"
+    const val ktor = "2.0.0"
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -17,25 +17,25 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-                api("io.ktor:ktor-client-core:2.0.0")
-                implementation("io.ktor:ktor-client-okhttp:2.0.0")
-                implementation("io.ktor:ktor-client-content-negotiation:2.0.0")
-                implementation("io.ktor:ktor-serialization-kotlinx-json:2.0.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.serialization}")
+                api("io.ktor:ktor-client-core:${Versions.ktor}")
+                implementation("io.ktor:ktor-client-content-negotiation:${Versions.ktor}")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:${Versions.ktor}")
             }
         }
 
         val jvmMain by getting {
+            dependsOn(commonMain)
             dependencies {
-                implementation("io.ktor:ktor-client-okhttp:2.0.0")
+                implementation("io.ktor:ktor-client-okhttp:${Versions.ktor}")
             }
         }
 
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}")
                 implementation("org.mockito:mockito-core:4.5.1")
                 implementation("com.squareup.okhttp3:mockwebserver:${Versions.okhttp}")
                 implementation("org.assertj:assertj-core:3.22.0")
