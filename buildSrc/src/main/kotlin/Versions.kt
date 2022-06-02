0cb3154156f47b99657f0194b25c92779128f9e3
Luca Spinazzola <mspinluca@gmail.com>
2022-06-02 04:16:13 -0400

    update ktor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,6 +5,6 @@ object Versions {
     const val jupiter = "5.6.2"
     const val okhttp = "4.9.3"
     const val coroutines = "1.6.1"
-    const val ktor = "2.0.0"
+    const val ktor = "2.0.2"
     const val okio = "3.1.0"
 }
