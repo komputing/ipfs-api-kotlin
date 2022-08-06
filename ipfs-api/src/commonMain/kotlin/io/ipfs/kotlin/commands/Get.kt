361012a7844cf6276e58ef93fa9ccab55e8744fc
Luca Spinazzola <mspinluca@gmail.com>
2022-08-05 23:42:50 -0400

    Fix getting bytes

--- ipfs-api/src/commonMain/kotlin/io/ipfs/kotlin/commands/Get.kt
+++ ipfs-api/src/commonMain/kotlin/io/ipfs/kotlin/commands/Get.kt
@@ -2,6 +2,7 @@ package io.ipfs.kotlin.commands
 
 import io.ipfs.kotlin.IPFSConnection
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.utils.io.*
 
 class Get(val ipfs: IPFSConnection) {
@@ -11,14 +12,17 @@ class Get(val ipfs: IPFSConnection) {
      *
      * @param hash The hash of the content in base58.
      */
-    suspend fun cat(hash: String): String = ipfs.callCmd("cat/$hash").bodyAsText()
+    suspend fun cat(hash: String): String = catBytes(hash).decodeToString()
 
     /**
      * Cat IPFS content and return it as ByteArray.
      *
      * @param hash The hash of the content in base58.
      */
-    suspend fun catBytes(hash: String): ByteArray = ipfs.callCmd("cat/$hash").readBytes()
+    suspend fun catBytes(hash: String): ByteArray {
+        val r = ipfs.callCmd("cat?arg=$hash")
+        return r.readBytes()
+    }
 
     /**
      * Cat IPFS content and process it using ByteReadChannel.
@@ -27,7 +31,7 @@ class Get(val ipfs: IPFSConnection) {
      * @param handler Callback which handle processing the input stream. When the callback return the stream and the request body will be closed.
      */
     suspend fun catReadChannel(hash: String, handler: (stream: ByteReadChannel) -> Unit): Unit =
-        ipfs.callCmd("cat/$hash").let { response ->
+        ipfs.callCmd("cat?arg=$hash").let { response ->
             val channel = response.bodyAsChannel()
             handler.invoke(channel)
         }
