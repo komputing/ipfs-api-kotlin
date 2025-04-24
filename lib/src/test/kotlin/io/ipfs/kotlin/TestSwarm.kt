eda30856255ab858831f32cdbf0ab8be53854161
Dirk Jäckel <github@dirk.jaeckel.name>
2025-04-24 21:30:07 +0200

    Use http response to determine whether to use the error jsonParser or the result datatypes's parser

--- lib/src/test/kotlin/io/ipfs/kotlin/TestSwarm.kt
+++ lib/src/test/kotlin/io/ipfs/kotlin/TestSwarm.kt
@@ -1,5 +1,7 @@
 package io.ipfs.kotlin
 
+import io.ipfs.kotlin.commands.KuboResult
+import io.ipfs.kotlin.model.Strings
 import okhttp3.mockwebserver.MockResponse
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -9,16 +11,38 @@ class TestSwarm : BaseIPFSWebserverTest() {
     @Test
     fun testSwarmConnect() {
         // setup
-        server.enqueue(MockResponse().setBody("{\"Strings\":[\"connect Qma8ddFEQWEU8ijWvdxXm3nxU7oHsRtCykAaVz8WUYhiKn success\"]}\n"))
+        server.enqueue(MockResponse().setBody("{\"Strings\":[\"success message\"]}\n").setResponseCode(200))
 
         // invoke
-        val addString = ipfs.swarm.connect("/ip4/137.184.243.187/tcp/3000/ws/p2p/Qma8ddFEQWEU8ijWvdxXm3nxU7oHsRtCykAaVz8WUYhiKn")
+        val connected = ipfs.swarm.connect("valid-address")
 
         // assert
-        assertThat(addString).isNotNull()
+        assertThat(connected).isNotNull()
+        assertThat(connected).isEqualTo(KuboResult.Success(Strings(listOf("success message"))))
+
+        assertThat(ipfs.lastError).isNull()
 
         val executedRequest = server.takeRequest()
-        assertThat(executedRequest.path).isEqualTo("/swarm/connect?args=/ip4/137.184.243.187/tcp/3000/ws/p2p/Qma8ddFEQWEU8ijWvdxXm3nxU7oHsRtCykAaVz8WUYhiKn")
+        assertThat(executedRequest.path).isEqualTo("/swarm/connect?arg=valid-address")
+    }
 
+    @Test
+    fun testSwarmConnectWithError() {
+        // setup
+        server.enqueue(MockResponse().setBody("""{"Message":"error message","Code":0,"Type":"error"}""").setResponseCode(500))
+
+        // invoke
+        val connected = ipfs.swarm.connect("invalid-address")
+
+        // assert
+        assertThat(connected).isNotNull()
+        assertThat(connected).isEqualTo(KuboResult.Failure("error message"))
+
+        assertThat(ipfs.lastError).isNotNull()
+        assertThat(ipfs.lastError!!.Message).isEqualTo("error message")
+        assertThat(ipfs.lastError!!.Code).isEqualTo(0)
+
+        val executedRequest = server.takeRequest()
+        assertThat(executedRequest.path).isEqualTo("/swarm/connect?arg=invalid-address")
     }
 }
